d2f7743905a4782252169c32f04e05a707fb2b6b
TylerTurpin10 <820660@mystma.org>
2022-09-21 09:05:52 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-#WRITE YOUR CODE IN THIS FILE
+def greaterThan(x,y):
+    if x>y:
+        return True
+    else:
+        return False
+
+def equalto(x,y):
+    if x == y:
+        return True
+    else:
+        return False
+
+def lessOrEqual(x,y):
+    if x < y:
+        return True
+    else:
+        return False
+
+def lessOrEqual(x,y):
+    if x == y:
+        return True
+    else:
+        return False
+
+def lessOrEqual(x,y):
+    if x>y:
+        return False
+    else:
+        return False
